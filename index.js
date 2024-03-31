af056d963775eb1f7a6d0182da0a416702153ee2
Glitch (fascinated-noiseless-gemini) <none>
2024-03-31 12:54:46 +0000

    👝🚿 Checkpoint ./index.js:60282313/312

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bp=require('body-parser');
+const db=require('mongoose');
 require('dotenv').config()
+
+db.connect(process.env.LINK,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>console.log("connected"))
+.catch(()=>console.log("error connecting"));
 
 app.use(cors())
 app.use(express.static('public'))
